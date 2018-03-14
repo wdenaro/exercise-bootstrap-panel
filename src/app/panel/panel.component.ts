135e07b7a54b960a79c1b72cdaccd4b2d3dcb25b
Wesley Denaro <Wesley.Denaro@iCloud.com>
2018-03-14 10:30:02 -0400

    Side exercise to demonstrate <ng-content>. Initial Commit.

---
+++ src/app/panel/panel.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bootstrap-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.scss']
+})
+export class PanelComponent {
+}
